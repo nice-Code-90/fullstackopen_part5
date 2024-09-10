48b923d2c32aede381eac4eeebfef441d7a5a5cc
Milan Mazik <mazik1990@hotmail.com>
2024-09-10 17:16:41 +0200

    5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author} {blog.url}
-  </div>
-);
+import { useState } from "react";
 
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  const [buttonContent, setButtonContent] = useState("view");
+
+  const handleViewChange = () => {
+    if (buttonContent === "view") {
+      setButtonContent("hide");
+    } else {
+      setButtonContent("view");
+    }
+  };
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button onClick={handleViewChange}>{buttonContent}</button>
+      </div>
+      {buttonContent === "hide" && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes} <button>like</button>
+          </div>
+          <div> {blog.author}</div>
+        </div>
+      )}
+    </div>
+  );
+};
 export default Blog;
